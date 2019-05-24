af5b3c28f9be1e414eab319196f19403c92e70ed
tirans3 <tirans3@mail.ru>
2019-05-25 01:45:54 +0400

    Add new folder and class ViewModels/ProductListViewModel

---
+++ SayCheese/SayCheese/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SayCheese.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
