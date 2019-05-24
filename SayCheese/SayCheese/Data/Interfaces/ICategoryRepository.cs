132f6ce6a42c25bd3bae9de6c06e72bb38900766
tirans3 <tirans3@mail.ru>
2019-05-24 15:33:17 +0400

    Add interface

---
+++ SayCheese/SayCheese/Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using SayCheese.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.Data.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Categories { get; }
+    }
+}
+
+
---
+++ SayCheese/SayCheese/Data/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using SayCheese.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.Data.Interfaces
+{
+    interface IProductRepository
+    {
+            IEnumerable<Product> Products { get; }
+            IEnumerable<Product> PreferredProducts { get; }
+            Product GetProductById(int productId);
+
+    }
+}
