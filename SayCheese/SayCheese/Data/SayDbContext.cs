c14b731dde86a8b693bd5e7252a2a045a5849c2e
tirans3 <tirans3@mail.ru>
2019-05-25 22:31:44 +0400

    Add new class SayDbContext for create database and tables

---
+++ SayCheese/SayCheese/Data/SayDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SayCheese.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.Data
+{
+    public class SayDbContext:DbContext
+    {
+        public SayDbContext(DbContextOptions<SayDbContext> options):base(options)
+        {
+        }
+        public DbSet<Product> Prosucts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
