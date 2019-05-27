5553b6967605ffa9879d149628d255af3f204d62
tirans3 <tirans3@mail.ru>
2019-05-27 18:50:30 +0400

    Add new controller

---
+++ SayCheese/SayCheese/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SayCheese.Data.Interfaces;
+using SayCheese.ViewModels;
+
+namespace SayCheese.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PreferredProducts = _productRepository.PreferredProducts
+            };
+            return View(homeViewModel);
+        }
+    }
+}
