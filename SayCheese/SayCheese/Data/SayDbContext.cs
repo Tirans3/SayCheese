62d15903462fba839cb030f51761d217804c2dcc
tirans3 <tirans3@mail.ru>
2019-05-27 16:19:23 +0400

    Add new property ShoppingCartItems for new table

--- SayCheese/SayCheese/Data/SayDbContext.cs
+++ SayCheese/SayCheese/Data/SayDbContext.cs
@@ -10,5 +10,6 @@ namespace SayCheese.Data
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
