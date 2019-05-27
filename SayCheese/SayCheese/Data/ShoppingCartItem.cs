c20be8318b93d78c6d869901f86c7425077cc7a0
tirans3 <tirans3@mail.ru>
2019-05-27 12:50:15 +0400

    Add new class ShoppingCartItem

---
+++ SayCheese/SayCheese/Data/ShoppingCartItem.cs
@@ -0,0 +1,13 @@
+﻿using SayCheese.Data.Models;
+
+namespace SayCheese.Data
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCartId { get; set; }
+
+    }
+}
