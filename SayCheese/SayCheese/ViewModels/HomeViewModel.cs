5704bd992978513e375e7876a6f0e6c916a7be77
tirans3 <tirans3@mail.ru>
2019-05-27 18:49:28 +0400

    Add new viewmodel

---
+++ SayCheese/SayCheese/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SayCheese.Data.Models;
+using System.Collections.Generic;
+
+namespace SayCheese.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Product> PreferredProducts  { get; set; }
+    }
+}
