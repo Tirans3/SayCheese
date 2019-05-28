609a6a509362fa46f2484a33ca253c5a33ca78b2
tirans3 <tirans3@mail.ru>
2019-05-28 04:08:18 +0400

    Add new class ViewComponent for  ShoppinCart

---
+++ SayCheese/SayCheese/Components/ShoppingCartSummary.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SayCheese.Data;
+using SayCheese.Data.Models;
+using SayCheese.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = new List<ShoppingCartItem> { new ShoppingCartItem() };//_shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+
+
+    }
+}
