362cd58cc62ffa00a0c7fd89ea3cd7a0a8d48744
tirans3 <tirans3@mail.ru>
2019-05-29 00:52:09 +0400

    Create new ViewComponent

---
+++ SayCheese/SayCheese/Components/CategoryMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SayCheese.Data.Interfaces;
+using System.Linq;
+
+namespace SayCheese.Components
+{
+    public class CategoryMenu: ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(p => p.CategoryName);
+            return View(categories);
+        }
+    }
+}
