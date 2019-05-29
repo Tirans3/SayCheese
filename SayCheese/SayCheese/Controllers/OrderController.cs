7b3eb95bec338aa4794595f6b580a841e6f0462d
tirans3 <tirans3@mail.ru>
2019-05-29 14:11:57 +0400

    Add new controller

--- SayCheese/SayCheese/Controllers/OrderController.cs
+++ SayCheese/SayCheese/Controllers/OrderController.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SayCheese.Data.Interfaces;
+using SayCheese.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SayCheese.Controllers
 {
-    public class OrderController
+    public class OrderController:Controller
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+      
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        
     }
 }
