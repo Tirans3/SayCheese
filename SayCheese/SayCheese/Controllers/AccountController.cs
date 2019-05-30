2606f398c70594a6abdf2e130fa13d8a216b235c
tirans3 <tirans3@mail.ru>
2019-05-30 14:07:39 +0400

    Add new controller For registr,  log in/out,

---
+++ SayCheese/SayCheese/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SayCheese.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.Controllers
+{
+        public class AccountController : Controller
+        {
+            private readonly UserManager<IdentityUser> _userManager;
+            private readonly SignInManager<IdentityUser> _signInManager;
+
+            public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+            }
+
+            public IActionResult Login(string returnUrl)
+            {
+                return View(new LoginViewModel
+                {
+                    ReturnUrl = returnUrl
+                });
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+            {
+                if (!ModelState.IsValid)
+                    return View(loginViewModel);
+
+                var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+                if (user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, loginViewModel.RememberMe, false);
+                    if (result.Succeeded)
+                    {
+                        if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                            return RedirectToAction("Index", "Home");
+
+                        return Redirect(loginViewModel.ReturnUrl);
+                    }
+                }
+
+                ModelState.AddModelError("", "Username/password not found");
+                return View(loginViewModel);
+
+            }
+        }
+    }
+
