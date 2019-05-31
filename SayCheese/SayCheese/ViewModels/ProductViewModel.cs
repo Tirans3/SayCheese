903a4602699e64c2660663886c1d044f3ae9d1e7
tirans3 <tirans3@mail.ru>
2019-06-01 01:54:03 +0400

    Add new view model

---
+++ SayCheese/SayCheese/ViewModels/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayCheese.ViewModels
+{
+    public class ProductViewModel
+    {
+            public int ProductId { get; set; }
+            public string Name { get; set; }
+            public string ShortDescription { get; set; }
+            public decimal Price { get; set; }
+            public string ImageUrl { get; set; }
+        
+    }
+}
